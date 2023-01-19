d4ad1cfd5a2a941df10e6f53420327e1d05bd406
M.S <maha.hus.sallam@gmail.com>
2023-01-19 08:04:39 +0200

    Update GeolocationDisp.tsx

--- src/components/Geolocation/GeolocationDisp.tsx
+++ src/components/Geolocation/GeolocationDisp.tsx
@@ -1,4 +1,11 @@
-export {}
+import { Geolocation } from "./Geolocation";
 
-
-//some code that takes in props and displays the current weather based on coordinants which i successfully called
+export const GeolocationDisp = () => {
+    return (
+        <article className="p-8 bg-slate-800 min-h-screen text-slate-200 max-w-3xl mx-auto">
+            <h1 className="text-3xl font-bold my-4">We need to know your location first </h1>
+            <h2 className="text-xl font-bold my-3 mt-4">to continue, turn on device location</h2>
+            <Geolocation />
+        </article>
+    );
+};
