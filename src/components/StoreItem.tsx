6a9e55d5fbfe81625b02f7df10334297040568bd
M.S <maha.hus.sallam@gmail.com>
2023-01-19 13:36:29 +0200

    Update StoreItem.tsx

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -18,19 +18,20 @@ export function StoreItem({ id, name, price, barcode, imgUrl }: StoreItemProps) 
         decreaseCartQuantity,
         removeFromCart } = useShoppingCart()
     const quantity = getItemQuantity(id)
+
     return (
         <Card className="h-100">
             <Card.Img
                 variant="top"
                 src={imgUrl}
-                height="600px"
+                height="500px"
                 style={{ objectFit: "cover" }}
             />
             <Card.Body className="d-flex flex-column">
                 <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-                    <span className="fs-2">{name}</span>
+                    <span className="fs-3">{name}</span>
 
-                    <span className="fs-2 text-muted">{formatCurrency
+                    <span className="fs-3 text-muted">{formatCurrency
                         (price)}</span>
                 </Card.Title>
                 <div>
