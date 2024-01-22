1d4d0e92a451f75282d8ff27dde4031165cfac46
Mhussein27 <maha.hus.sallam@gmail.com>
2024-01-22 12:13:25 +0200

    implement lazyloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
-import { Home } from "./pages/Home"
-import { Store } from "./pages/Store"
-import { Location } from "./pages/Location"
-import  Scan  from "./pages/Scan"
 import { Navbar } from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
+const Home = lazy(() => import('./pages/Home'));
+const Store = lazy(() => import('./pages/Store').then(module => ({ default: module.Store })));
+const Location = lazy(() => import('./pages/Location'));
+const Scan = lazy(() => import('./pages/Scan'));
 
 function App() {
   return (
     <ShoppingCartProvider>
       <Navbar/>
       <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/Location" element={<Location />} />
-          <Route path="/Scan" element={<Scan />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/Location" element={<Location />} />
+            <Route path="/Scan" element={<Scan />} />
+          </Routes>
+        </Suspense>
       </Container>
     </ShoppingCartProvider>
   )
 }
 
-export default App
+export default App;
